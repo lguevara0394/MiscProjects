000485110b06ef2cd5dc03576291baff05747aa7
Laura A Guevara Salazar <73564242+lguevara0394@users.noreply.github.com>
2025-07-24 09:50:00 -0600

    Add files via upload

---
+++ Chatbox.py
@@ -0,0 +1,12 @@
+def chatbot():
+    print("Hello, What is your name?")
+    name = input()
+    print(f"Nice to meet you, {name}!")
+    print("How old are you?")
+    age = input()
+    bot_age = 3
+    age_difference = age - bot_age
+    print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+    print("What is your favorite color?")
+    color = input()
+    print(f"Oh, {color} is a beautiful color!")
